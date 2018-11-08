d7ee8ad71c23640ca4cc67356c7afb2dce0a2fea
Jake Knowles <jakeyknowles@hotmail.co.uk>
2018-11-08 09:57:37 +0000

    Additional burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,6 +6,8 @@ const burger = (props) => {
   return (
     <div className="burger">
       <BurgerIngredient type="bread-top"/>
+      <BurgerIngredient type="cheese"/>
+      <BurgerIngredient type="meat"/>
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
