04787ba9043873be03763f958d6edab88b42b786
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-01-29 14:24:58 +0000

    Add a higher order component to handle errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Model from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -143,4 +144,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Model from '../components/UI/Modal/Modal';
+
+//This is grabbing the component that is param of the function and wrapping it with thhe model to show if anything goes wrong
+//Is a class inside the function that is annoymous as it is never named. Using this to access the axios instance on componentDidMount
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends React.Component {
+    state = {
+      error: null
+    };
+    
+    componentDidMount () {
+      axios.interceptors.request.use(req => {
+        this.setState({
+          error: null
+        });
+        return req;
+      });
+      axios.interceptors.response.use(res => res, error => {
+        this.setState({error: error});
+      });
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({error: null});
+    };
+
+    render() {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...this.props} />
+          <Model show={this.state.error} modelClose={this.errorConfirmedHandler}>{this.state.error ? this.state.error.message : null}</Model>
+        </React.Fragment>
+        
+      )
+    }
+  }
+}
+
+export default withErrorHandler;
